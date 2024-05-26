4412d1806c574b6f8565cb6d295f77bc8bcf5e35
tchatua <tchatua@hotmail.com>
2024-05-26 01:26:47 -0400

    SNS file updated

--- Terraform_CODE/sns.tf
+++ Terraform_CODE/sns.tf
@@ -2,6 +2,15 @@
 # Terraform aws create sns topic
 resource "aws_sns_topic" "user_updates" {
   name = "${var.name}-sns-topic"
+  tags = {
+    Name          = "${var.name}-sns-topic"
+    Folder        = var.folder
+    Author        = var.author
+    Project       = var.project
+    ManagedBy     = var.managedby
+    Owner         = var.owner
+    Administrator = var.administrator
+  }
 }
 
 # Create an sns topic subscription
@@ -19,5 +28,5 @@ resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
   topic_arn = aws_sns_topic.user_updates.arn
   protocol  = var.protocol_sns
   endpoint  = var.operator_email_sns
+  
 }
-
